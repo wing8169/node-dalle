a455996c5b205fb0253e2d9a1de9d7393727e2d3
Jia Xiong <wing8169@users.noreply.github.com>
2023-11-20 22:09:29 +0800

    Integrate with openai image generation with prompt

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 import template from "./index.marko";
 
+// sample interface
+// export interface Input {
+//   src: string;
+// }
+
 export default (req: Request, res: Response) => {
-  res.marko(template as any, {
-    src: "https://cdn.vectorstock.com/i/preview-1x/65/30/default-image-icon-missing-picture-page-vector-40546530.jpg",
-  });
+  res.marko(template as any, {});
 };
--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,34 @@
+// env name: OPENAPI_SECRET
+
 import { Router, Request, Response } from "express";
 import indexPage from "./components/index.ts";
-import OpenAI from "openai";
-import "dotenv/config";
 import template from "./components/img-content/index.marko";
+import "dotenv/config";
+import OpenAI from "openai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAPI_SECRET,
+  apiKey: process.env.OPENAPI_SECRET, // defaults to process.env["OPENAI_API_KEY"]
 });
 
 export const generateImage = async (req: Request, res: Response) => {
-  console.log(req.body);
-  let imageUrl: string | undefined = undefined;
-  // generate image
+  const prompt = req.body.prompt ?? "";
+  let imageUrl = "";
   try {
     const response = await openai.images.generate({
       model: "dall-e-3",
-      prompt: req.body.prompt ?? "beautiful white girl",
+      prompt,
       n: 1,
       size: "1024x1024",
     });
     imageUrl = response.data[0].url ?? "";
   } catch (err: any) {
-    res.send({ err: err.toString() });
+    console.log(err);
+    res.send({ error: err.toString() });
   }
-  console.log("generated", imageUrl);
-  // const imageUrl = undefined;
   res.marko(template as any, {
     src:
       imageUrl ??
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT54sU7bc_QAOL63miw2XRTVXAjsdK8vA9W08LAMog&s",
+      "https://images.unsplash.com/photo-1481349518771-20055b2a7b24?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tfGVufDB8fDB8fHww",
   });
 };
 
