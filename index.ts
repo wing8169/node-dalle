bb31c1221e3c5e64f5b9239687ce0f51e798b615
Chin Jia Xiong <jiaxiong@Chins-MacBook-Pro.local>
2023-11-21 11:39:06 +0800

    Fixed build and run script

--- index.ts
+++ index.ts
@@ -16,47 +16,51 @@ declare global {
   }
 }
 
-const devEnv = "development";
-const { NODE_ENV = devEnv, PORT = 3000 } = process.env;
-console.time("Start");
+const runSever = async () => {
+  const devEnv = "development";
+  const { NODE_ENV = devEnv, PORT = 3000 } = process.env;
+  console.time("Start");
 
-const app = express()
-  .use(compressionMiddleware()) // Enable gzip compression for all HTTP responses.
-  // @ts-ignore
-  .use(markoMiddleware());
-
-if (NODE_ENV === devEnv) {
-  const { createServer } = await import("vite");
-  const devServer = await createServer({
-    appType: "custom",
-    server: { middlewareMode: true },
-  });
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(devServer.middlewares);
-  app.use(async (req, res, next) => {
-    try {
-      const { router } = await devServer.ssrLoadModule("./src/index.ts");
-      router(req, res, handleNext);
-    } catch (err) {
-      handleNext(err);
-    }
-
-    function handleNext(err: any) {
-      if (err) devServer.ssrFixStacktrace(err);
-      next(err);
-    }
-  });
-} else {
-  app
-    .use("/assets", express.static("dist/assets")) // Serve assets generated from vite.
+  const app = express()
+    .use(compressionMiddleware()) // Enable gzip compression for all HTTP responses.
     // @ts-ignore
-    .use((await import("dist/index.js")).router);
-}
+    .use(markoMiddleware());
 
-// app.post("/images", generateImage);
+  if (NODE_ENV === devEnv) {
+    const { createServer } = await import("vite");
+    const devServer = await createServer({
+      appType: "custom",
+      server: { middlewareMode: true },
+    });
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(devServer.middlewares);
+    app.use(async (req, res, next) => {
+      try {
+        const { router } = await devServer.ssrLoadModule("./src/index.ts");
+        router(req, res, handleNext);
+      } catch (err) {
+        handleNext(err);
+      }
 
-await once(app.listen(PORT), "listening");
+      function handleNext(err: any) {
+        if (err) devServer.ssrFixStacktrace(err);
+        next(err);
+      }
+    });
+  } else {
+    app
+      .use("/assets", express.static("./dist/assets")) // Serve assets generated from vite.
+      // @ts-ignore
+      .use((await import("./dist/index.js")).router);
+  }
 
-console.timeEnd("Start");
-console.log(`Env: ${NODE_ENV}`);
-console.log(`Address: http://localhost:${PORT}`);
+  // app.post("/images", generateImage);
+
+  await once(app.listen(PORT), "listening");
+
+  console.timeEnd("Start");
+  console.log(`Env: ${NODE_ENV}`);
+  console.log(`Address: http://localhost:${PORT}`);
+};
+
+runSever();
